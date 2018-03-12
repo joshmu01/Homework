8a3ee868bf0dcc4ca7e959f7678ae49b2d1a3b91
joshmu01 <joshmu01@noa.nintendo.com>
2018-03-11 18:54:40 -0700

    Homework 3 Complete

--- CSHP220C_Homework/SecondWindow.xaml.cs
+++ CSHP220C_Homework/SecondWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace CSHP220C_Homework
 {
@@ -19,15 +20,31 @@ namespace CSHP220C_Homework
     /// </summary>
     public partial class SecondWindow : Window
     {
+        private string columnToSort = "";
+        private ListSortDirection sortDirection = ListSortDirection.Ascending;
+
         public SecondWindow()
         {
             InitializeComponent();
             listView_UserInfo.ItemsSource = MainWindow.users;
+            columnToSort = columnHeader_UserName.Tag.ToString();
+            listView_UserInfo.Items.SortDescriptions.Add(new SortDescription(columnToSort, sortDirection));
         }
 
         private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
         {
+            string columnClicked = (sender as GridViewColumnHeader).Tag.ToString();
+            listView_UserInfo.Items.SortDescriptions.Clear();
 
+            ListSortDirection newDirection = ListSortDirection.Ascending;
+            if (columnToSort == columnClicked && sortDirection == newDirection)
+            {
+                newDirection = ListSortDirection.Descending;
+            }
+
+            sortDirection = newDirection;
+            columnToSort = columnClicked;
+            listView_UserInfo.Items.SortDescriptions.Add(new SortDescription(columnToSort, sortDirection));
         }
     }
 }
