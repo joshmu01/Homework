5b1d8091f4e4256a8fe34be0ac2f09ee64118896
joshmu01 <joshmu01@noa.nintendo.com>
2018-04-17 18:17:18 -0700

    CSHP230C - Homework Assignment 1

---
+++ CSHP230C_Homework1/BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CardForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CardForm(Models.CardDetails cardDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("CardSent", cardDetails);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ CSHP230C_Homework1/BirthdayCard/Models/CardDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BirthdayCard.Models
+{
+    public class CardDetails
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string ToName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address.")]
+        public string ToEmail { get; set; }
+
+        [Required(ErrorMessage = "Please enter the recipient's name.")]
+        public string FromName { get; set; }
+
+        [Required(ErrorMessage = "Please enter the recipient's email address.")]
+        public string FromEmail { get; set; }
+
+        [Required(ErrorMessage = "Please enter a birthday message.")]
+        public string BirthdayMessage { get; set; }
+    }
+}
